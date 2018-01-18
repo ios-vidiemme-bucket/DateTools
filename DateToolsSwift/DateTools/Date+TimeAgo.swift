a2894c8576429be20df684c532661094110e461e
Enrico Palleva <enrico.palleva@vidiemme.it>
2018-01-18 11:39:42 +0100

    Date+TimeAgo bug fixed (yesterday bug)

--- DateToolsSwift/DateTools/Date+TimeAgo.swift
+++ DateToolsSwift/DateTools/Date+TimeAgo.swift
@@ -112,7 +112,7 @@ public extension Date {
         else if (components.day! >= 2) {
             return self.logicalLocalizedStringFromFormat(format: "%%d %@days ago", value: components.day!)
         }
-        else if (isYesterday) {
+        else if (isYesterday || components.day! == 1) {
             if (numericDates) {
                 return DateToolsLocalizedStrings("1 day ago");
             }
